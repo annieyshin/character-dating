4fdcde3fc45841e086dec291c4e7938f52e74805
Annie Shin and Kyle Parsons <kyle@>
2018-05-31 11:19:15 -0700

    add select boxes and vars

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// BUSINESS LOGIC
+
+var foods = $("#foods").val();
+var foods = $("#music").val();
+var foods = $("#color").val();
+var foods = $("#animal").val();
+var foods = $("#show/movie").val();
+var foods = $("#book").val();
+
+
+// USER INTERFACE
